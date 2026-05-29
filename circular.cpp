9ea5a160b35663a9ddef231ff1a6ff4b92759de4
AnggaWdnt <widiangga94@gmail.com>
2026-05-29 17:41:55 +0700

    Menambahkan tampilan queue normal

--- circular.cpp
+++ circular.cpp
@@ -80,4 +80,17 @@ private:
         cout << "Queue is empty\n";
         return;
     }
+
+    cout << "\nElement in the queue are...\n";
+
+    if (FRONT_Position < REAR_Position)
+    {
+        while (FRONT_Position <= REAR_Position)
+        {
+            cout << queue_array[FRONT_Position] << " ";
+            FRONT_Position++;
+        }
+
+        cout << endl;
     }
+        }
